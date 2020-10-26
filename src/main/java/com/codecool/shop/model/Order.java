a4880ad6697cade9662330978e098351dfa73b40
RobertMatlag <robert.matlag@gmail.com>
2020-10-26 18:28:37 +0100

    Add Order model class

---
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -0,0 +1,21 @@
+package com.codecool.shop.model;
+
+public class Order extends BaseModel {
+    private boolean paid;
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void markPaid() {
+        this.paid = true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("id: %1$d, " +
+                        "paid status: %2$b",
+                this.id,
+                this.paid);
+    }
+}
