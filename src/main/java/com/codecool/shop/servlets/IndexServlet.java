b5cb47300836643645a858097b5103653cfe4529
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-26 20:54:08 +0100

    Add sampleProducts list to send via Thymeleaf by index endpoint

--- src/main/java/com/codecool/shop/servlets/IndexServlet.java
+++ src/main/java/com/codecool/shop/servlets/IndexServlet.java
@@ -3,6 +3,7 @@ package com.codecool.shop.servlets;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.logic.enumerators.Category;
 import com.codecool.shop.logic.enumerators.Supplier;
+import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -12,7 +13,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @WebServlet(urlPatterns = {"/"})
@@ -21,10 +24,18 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         WebContext context = new WebContext(request, response, request.getServletContext());
 
+        List<Product> sampleProducts = Arrays.asList(
+                new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", Category.TABLET, Supplier.AMAZON),
+                new Product("Lenovo IdeaPad Mix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", Category.TABLET, Supplier.LENOVO),
+                new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", Category.TABLET, Supplier.AMAZON)
+        );
+
+        // List<Product> products = ProductLogic.getAllProductsFromDatabase("default", "default");
+
         Map<String, Object> params = new HashMap<>();
         params.put("supplier", Supplier.values());
         params.put("category", Category.values());
-        params.put("products", );
+        params.put("products", sampleProducts);
         context.setVariables(params);
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
