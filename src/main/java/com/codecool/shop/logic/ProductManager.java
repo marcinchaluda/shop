053ddd851ac6f72af0a5a148daa11798d61d6466
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-26 19:15:29 +0100

    Add not implemented ProductManager to hold logic element with Products

---
+++ src/main/java/com/codecool/shop/logic/ProductManager.java
@@ -0,0 +1,41 @@
+package com.codecool.shop.logic;
+
+import com.codecool.shop.logic.enumerators.SortType;
+import com.codecool.shop.model.Product;
+
+import java.util.List;
+
+public class ProductManager extends BusinessLogic {
+
+    public static Product getElementFromDatabase() {
+        // TODO
+        throw new RuntimeException("Not implemented yet!");
+    }
+
+    public static void setProductInDatabase(Product product) {
+        // TODO
+        throw new RuntimeException("Not implemented yet!");
+    }
+
+    public static List<Product> getAllProductsFromDatabase(SortType sortType, String element) {
+        // TODO
+        //  element -> name of supplier or name of category
+
+        switch (sortType) {
+            case SUPPLIER:
+                break;
+            case CATEGORY:
+                break;
+            default:
+                break;
+        }
+
+        throw new RuntimeException("Not implemented yet!");
+    }
+
+    public static void setAllProductsInDatabase(List<Product> products) {
+        // TODO
+        throw new RuntimeException("Not implemented yet!");
+    }
+
+}
