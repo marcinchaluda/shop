9616d6133aefb5c494b3a4eb142a92b8133edba6
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-26 20:29:23 +0100

    Refactor some of code

---
+++ src/main/java/com/codecool/shop/logic/enumerators/Category.java
@@ -0,0 +1,8 @@
+package com.codecool.shop.logic.enumerators;
+
+public enum Category {
+    PHONE,
+    TABLET,
+    LAPTOP,
+    PERSONAL_COMPUTER
+}
--- src/main/java/com/codecool/shop/servlets/IndexServlet.java
+++ src/main/java/com/codecool/shop/servlets/IndexServlet.java
@@ -1,6 +1,8 @@
 package com.codecool.shop.servlets;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.logic.enumerators.Category;
+import com.codecool.shop.logic.enumerators.Supplier;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -10,12 +12,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(urlPatterns = {"/"})
 public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         WebContext context = new WebContext(request, response, request.getServletContext());
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("supplier", Supplier.values());
+        params.put("category", Category.values());
+        params.put("products", );
+        context.setVariables(params);
+
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         engine.process("product/index.html", context, response.getWriter());
     }
