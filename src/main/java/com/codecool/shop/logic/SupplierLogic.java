9bd1b6c00c5e5c41622c6bdd2ec6b5b54e6925da
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-27 18:04:39 +0100

    Add SupplierLogic class to handle operations

---
+++ src/main/java/com/codecool/shop/logic/SupplierLogic.java
@@ -0,0 +1,29 @@
+package com.codecool.shop.logic;
+
+import com.codecool.shop.model.Supplier;
+
+import java.util.List;
+
+public class SupplierLogic implements BusinessLogic<Supplier> {
+
+
+    @Override
+    public void addElement(Supplier supplier) {
+        throw new RuntimeException("Not implemented yet!");
+    }
+
+    @Override
+    public void updateElement(Supplier supplier) {
+        throw new RuntimeException("Not implemented yet!");
+    }
+
+    @Override
+    public Supplier getElement(int id) {
+        throw new RuntimeException("Not implemented yet!");
+    }
+
+    @Override
+    public List<Supplier> getAllElements() {
+        throw new RuntimeException("Not implemented yet!");
+    }
+}
