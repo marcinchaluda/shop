b35953f25d8e953c32e2e126ec1a877a7b603f04
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-27 18:22:38 +0100

    Add CategoryDao implementation

--- src/main/java/com/codecool/shop/dao/implementation/CategoryDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/CategoryDaoJdbc.java
@@ -4,6 +4,8 @@ import com.codecool.shop.dao.SimpleInOutDao;
 import com.codecool.shop.model.Category;
 
 import javax.sql.DataSource;
+import java.sql.*;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CategoryDaoJdbc implements SimpleInOutDao<Category> {
@@ -16,16 +18,58 @@ public class CategoryDaoJdbc implements SimpleInOutDao<Category> {
 
     @Override
     public void add(Category category) {
-
+        try (Connection connection = dataSource.getConnection()) {
+            String sqlQuery = "INSERT INTO product_category (name) VALUES (?);";
+            PreparedStatement statement = connection.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, category.getName());
+            statement.executeUpdate();
+            ResultSet result = statement.getGeneratedKeys();
+            result.next();
+            category.setId(result.getInt(1));
+        } catch (SQLException error) {
+            throw new RuntimeException("Error while adding a new Category.", error);
+        }
     }
 
     @Override
     public Category get(int id) {
-        return null;
+        try (Connection connection = dataSource.getConnection()) {
+            String sqlQuery = "SELECT id, name FROM product_category WHERE id = ?";
+            PreparedStatement statement = connection.prepareStatement(sqlQuery);
+            statement.setInt(1, id);
+            statement.executeUpdate();
+            ResultSet result = statement.getGeneratedKeys();
+            if (result.next()) {
+                return getCategory(result);
+            }
+            return null;
+        } catch (SQLException error) {
+            throw new RuntimeException("Error while getting a Supplier.", error);
+        }
     }
 
     @Override
     public List<Category> getAll() {
-        return null;
+        try (Connection connection = dataSource.getConnection()) {
+            List<Category> categories = new LinkedList<>();
+            String sqlQuery = "SELECT id, name, description, country FROM supplier";
+            PreparedStatement statement = connection.prepareStatement(sqlQuery);
+            statement.executeUpdate();
+            ResultSet result = statement.getGeneratedKeys();
+            while (result.next()) {
+                categories.add(getCategory(result));
+            }
+            return categories;
+        } catch (SQLException error) {
+            throw new RuntimeException("Error while getting all Suppliers.", error);
+        }
+    }
+
+    private Category getCategory(ResultSet result) throws SQLException {
+        int id = result.getInt("id");
+        String name = result.getString("name");
+        Category category = new Category(name);
+        category.setId(id);
+        return category;
     }
 }
