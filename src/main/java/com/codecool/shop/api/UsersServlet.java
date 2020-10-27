6fd7f7e60b9b5a8c45f0f00e485027d3242ca255
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-27 10:59:37 +0100

    Add implemented UsersServlet class

---
+++ src/main/java/com/codecool/shop/api/UsersServlet.java
@@ -0,0 +1,37 @@
+package com.codecool.shop.api;
+
+import com.codecool.shop.logic.UserLogic;
+import com.codecool.shop.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/api/users/*"})
+public class UsersServlet extends HttpServlet {
+
+    UserLogic userLogic = new UserLogic();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HelpServlet.getElementsBasedOnUriId(request, response, userLogic, User.class);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HelpServlet.updateOneElementInBusinessLogic(request, response, userLogic, User.class);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HelpServlet.addOneElementToBusinessLogic(request, response, userLogic, User.class);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HelpServlet.removeOneElementToBusinessLogic(request, response, userLogic, User.class);
+    }
+}
