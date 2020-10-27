022d810b1a114482228ab742a097965a89463468
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-27 18:22:54 +0100

    Remove unused lines in IndexServlet

--- src/main/java/com/codecool/shop/api/IndexServlet.java
+++ src/main/java/com/codecool/shop/api/IndexServlet.java
@@ -1,77 +1,15 @@
 package com.codecool.shop.api;
 
-import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.logic.enumerators.Category;
-import com.codecool.shop.logic.enumerators.Supplier;
-import com.codecool.shop.model.Product;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @WebServlet(urlPatterns = {"/"})
 public class IndexServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        WebContext context = new WebContext(request, response, request.getServletContext());
-
-        List<Product> sampleProducts = Arrays.asList(
-                new Product("Amazon Fire",
-                        "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.",
-                        49.9f,
-                        "USD",
-                        Category.TABLET,
-                        Supplier.AMAZON,
-                        ""),
-                new Product(
-                        "Lenovo IdeaPad Mix 700",
-                        "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.",
-                        479,
-                        "USD",
-                        Category.TABLET,
-                        Supplier.LENOVO,""),
-                new Product(
-                        "Amazon Fire HD 8",
-                        "Amazon's latest Fire HD 8 tablet is a great value for media consumption.",
-                        89,
-                        "USD",
-                        Category.TABLET,
-                        Supplier.AMAZON,
-                        "")
-        );
-
-        // List<Product> products = ProductLogic.getAllProductsFromDatabase("default", "default");
-
-        Map<String, Object> params = new HashMap<>();
-        params.put("suppliers", Supplier.values());
-        params.put("categories", Category.values());
-        params.put("products", sampleProducts);
-        context.setVariables(params);
-
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
-        engine.process("product/index.html", context, response.getWriter());
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("product/index.html");
     }
 }
-
-//        ProductDao productDataStore = ProductDaoMem.getInstance();
-//        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-//
-//        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-//        WebContext context = new WebContext(req, resp, req.getServletContext());
-//        context.setVariable("category", productCategoryDataStore.find(1));
-//        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-//        // // Alternative setting of the template context
-//        // Map<String, Object> params = new HashMap<>();
-//        // params.put("category", productCategoryDataStore.find(1));
-//        // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-//        // context.setVariables(params);
-//        engine.process("product/index.html", context, resp.getWriter());
