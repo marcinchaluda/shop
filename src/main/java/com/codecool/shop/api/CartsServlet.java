147fd966836f515776fe67400dfae390db515cc0
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-27 11:07:03 +0100

    Add implemented carts endpoints

---
+++ src/main/java/com/codecool/shop/api/CartsServlet.java
@@ -0,0 +1,37 @@
+package com.codecool.shop.api;
+
+import com.codecool.shop.logic.CartLogic;
+import com.codecool.shop.model.Cart;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/api/carts/*"})
+public class CartsServlet extends HttpServlet {
+
+    CartLogic cartLogic = new CartLogic();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HelpServlet.getElementsBasedOnUriId(request, response, cartLogic, Cart.class);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HelpServlet.updateOneElementInBusinessLogic(request, response, cartLogic, Cart.class);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HelpServlet.addOneElementToBusinessLogic(request, response, cartLogic, Cart.class);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HelpServlet.removeOneElementToBusinessLogic(request, response, cartLogic, Cart.class);
+    }
+}
