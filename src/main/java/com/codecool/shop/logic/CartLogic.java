b6d68c2c8b17fa77eac3c5f7ff6ea7f9da164a40
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-28 13:33:22 +0100

    Add implementation of CartLogic

--- src/main/java/com/codecool/shop/logic/CartLogic.java
+++ src/main/java/com/codecool/shop/logic/CartLogic.java
@@ -3,10 +3,11 @@ package com.codecool.shop.logic;
 import com.codecool.shop.dao.ModifyDao;
 import com.codecool.shop.dao.ShopDatabaseManager;
 import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.Product;
 
-import java.util.List;
+import java.util.Map;
 
-public class CartLogic implements NotSortable<Cart> {
+public class CartLogic implements BusinessLogic<Cart> {
     ModifyDao<Cart> cartDao = ShopDatabaseManager.Instance.getCartDao();
 
     private static CartLogic instance = null;
@@ -20,26 +21,33 @@ public class CartLogic implements NotSortable<Cart> {
 
     @Override
     public void addElement(Cart cart) {
-        throw new RuntimeException("Not implemented yet!");
+        cartDao.add(cart);
     }
 
     @Override
     public void updateElement(Cart cart) {
-        throw new RuntimeException("Not implemented yet!");
+        cartDao.update(cart);
     }
 
     @Override
-    public void removeElement(Cart element) {
-        throw new RuntimeException("Not implemented yet!");
+    public void removeElement(Cart cart) {
+        cartDao.remove(cart.getId());
     }
 
     @Override
     public Cart getElement(int id) {
-        throw new RuntimeException("Not implemented yet! - getElement " + id);
-    }
+        double totalPrice = 0;
+        int quantity = 0;
+        Cart cart = cartDao.get(id);
+        Map<Product, Integer> products = cart.getProductList();
 
-    @Override
-    public List<Cart> getAllElements() {
-        throw new RuntimeException("Not implemented yet!");
+        for (Map.Entry<Product, Integer> product : products.entrySet()) {
+            quantity += product.getValue();
+            totalPrice += product.getKey().getDefaultPrice();
+        }
+
+        cart.setQuantity(quantity);
+        cart.setTotalPrice(totalPrice);
+        return cart;
     }
 }
