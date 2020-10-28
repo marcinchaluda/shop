041bf6168c563af812c4c25c5e72e05355a63eeb
RobertMatlag <robert.matlag@gmail.com>
2020-10-28 19:06:05 +0100

    New functionality: chose number of products to add to cart

--- src/main/webapp/static/js/main_layout_generator.js
+++ src/main/webapp/static/js/main_layout_generator.js
@@ -1,4 +1,4 @@
-import { navButtonHandler } from "./buttons_handler.js";
+import {navButtonHandler} from "./buttons_handler.js";
 
 const container = document.querySelector(".container");
 
@@ -23,14 +23,14 @@ export const layoutGenerator = {
         const imageContainer = createImageElement.call(this);
         const cardHeader = createDescriptionElement.call(this, product);
         const cardPrice = createPriceElement.call(this, product);
-        const cardButton = createButtonElement.call(this);
+        const cardAddingBar = createAddingBar.call(this, product);
 
         cardDetails.id = product.id
         cardDetails.appendChild(cardTitle);
         cardDetails.appendChild(imageContainer);
         cardDetails.appendChild(cardHeader);
         cardDetails.appendChild(cardPrice);
-        cardDetails.appendChild(cardButton);
+        cardDetails.appendChild(cardAddingBar);
         card.appendChild(cardDetails);
 
         return card;
@@ -52,13 +52,14 @@ export const layoutGenerator = {
 function handleAddToCartButton() {
     const addToCartButtons = document.querySelectorAll(".btn");
     addToCartButtons.forEach(button => {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             const productId = document.querySelector(".card-details").id
             navButtonHandler.addProductToCart(productId)
             // navButtonHandler.showProduct(productId);
-            })
+        })
     })
 }
+
 function createNameElement(product) {
     const cardTitle = this.createElementWithClasses("h4", "card-title");
     cardTitle.innerHTML = product.name;
@@ -90,6 +91,17 @@ function createPriceElement(product) {
     return cardPrice;
 }
 
+function createAddingBar(product) {
+    const addingBar = this.createElementWithClasses("div", "adding-bar");
+    const cardButton = createButtonElement.call(this);
+    const quantity = createQuantityChooser.call(this, product);
+
+    addingBar.appendChild(cardButton);
+    addingBar.appendChild(quantity);
+
+    return addingBar;
+}
+
 function createButtonElement() {
     const cardButton = this.createElementWithClasses("div", "card-button");
     const button = this.createElementWithClasses("a", "btn");
@@ -100,4 +112,38 @@ function createButtonElement() {
     button.textContent += "Add to cart";
     cardButton.appendChild(button);
     return cardButton;
+}
+
+function createQuantityChooser(product) {
+    const quantity = this.createElementWithClasses("div", "quantity-container");
+
+    const inputQuantity = document.createElement("input");
+    inputQuantity.setAttribute("type", "number");
+    inputQuantity.setAttribute("name", "quantity");
+    inputQuantity.setAttribute("min", "1");
+    inputQuantity.setAttribute("value", "1");
+
+    const leftButton = document.createElement("button");
+    leftButton.innerText = "-";
+    leftButton.addEventListener("click", function () {
+        const input = document.querySelector(`div[id='${product.id}'] input`);
+        let quantityValue = input.getAttribute("value");
+        quantityValue = Math.max(quantityValue - 1, 1);
+        input.setAttribute("value", quantityValue.toString());
+    })
+
+    const rightButton = document.createElement("button");
+    rightButton.innerText = "+";
+    rightButton.addEventListener("click", function () {
+        const input = document.querySelector(`div[id='${product.id}'] input`);
+        let quantityValue = input.getAttribute("value");
+        quantityValue++;
+        input.setAttribute("value", quantityValue.toString());
+    })
+
+    quantity.appendChild(leftButton);
+    quantity.appendChild(inputQuantity);
+    quantity.appendChild(rightButton);
+
+    return quantity;
 }
