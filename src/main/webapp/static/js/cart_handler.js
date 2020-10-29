45e04c53cf8710a8428a85df3e354bb181fa313e
RobertMatlag <robert.matlag@gmail.com>
2020-10-29 21:48:41 +0100

    New functionality: it is possible change amount of product in cart

--- src/main/webapp/static/js/cart_handler.js
+++ src/main/webapp/static/js/cart_handler.js
@@ -1,6 +1,7 @@
 import {cartButtonHandler} from "./cart_button_handler.js";
 import {dataHandler} from "./data_handler.js";
-import { util } from "./util.js";
+import {util} from "./util.js";
+import {buttonHandler} from "./buttons_handler.js";
 
 const itemsContainer = document.querySelector("#items");
 const usernameTitleContainer = document.querySelector("#username-cart-title");
@@ -23,7 +24,7 @@ const createListOfItems = data => {
     for (let i = 0; i < data.products.length; i++) {
         const productDetails = data.products[i].product;
         const dataToInsert = {
-            product_id: data.products[i].id,
+            product_id: data.products[i].product.id,
             name: data.products[i].name,
             quantity: data.products[i].quantity,
             subTotalPrice: data.products[i].quantity * productDetails.unitPrice,
@@ -37,34 +38,53 @@ const createListOfItems = data => {
 }
 
 const addItemToDisplay = dataToInsert => {
-    const cardDetails = util.createElementWithClasses("div","card-details", "item", "flex-row");
-    const name = util.createElementWithClasses("p","item-name");
+    const cardDetails = util.createElementWithClasses("div", "card-details", "item", "flex-row");
+    const name = util.createElementWithClasses("p", "item-name");
     name.innerText = dataToInsert.name;
 
-    const quantityContainer = util.createElementWithClasses("div","quantity", "flex-row");
+    const quantityContainer = util.createElementWithClasses("div", "quantity", "flex-row");
+    quantityContainer.setAttribute("product_id", dataToInsert.product_id)
 
-    const minusButton = util.createElementWithClasses("a","plus-minus", "decrease-quantity");
+    const minusButton = util.createElementWithClasses("a", "plus-minus", "decrease-quantity");
     minusButton.setAttribute("href", "#");
-    const minusIcon = util.createElementWithClasses("i","fas", "fa-minus");
+    const minusIcon = util.createElementWithClasses("i", "fas", "fa-minus");
     minusButton.appendChild(minusIcon);
+
+    minusButton.addEventListener("click", function () {
+        const quantityNumber = document.querySelector(`div[product_id='${dataToInsert.product_id}'] p`);
+        let quantity = parseInt(quantityNumber.textContent);
+        quantity = Math.max((quantity - 1), 1);
+        quantityNumber.textContent = quantity.toString();
+
+        buttonHandler.updateProductInCart(1, dataToInsert.product_id, quantity) //TODO hardcode cartID
+    })
 
     const quantityValue = util.createElementWithClasses("p", "number");
     quantityValue.innerText = dataToInsert.quantity;
 
-    const plusButton = util.createElementWithClasses("a","plus-minus", "increase-quantity");
+    const plusButton = util.createElementWithClasses("a", "plus-minus", "increase-quantity");
     plusButton.setAttribute("href", "#");
-    const plusIcon = util.createElementWithClasses("i","fas", "fa-plus");
+    const plusIcon = util.createElementWithClasses("i", "fas", "fa-plus");
     plusButton.appendChild(plusIcon);
+
+    plusButton.addEventListener("click", function () {
+        const quantityNumber = document.querySelector(`div[product_id='${dataToInsert.product_id}'] p`);
+        let quantity = parseInt(quantityNumber.textContent);
+        quantity = quantity + 1;
+        quantityNumber.textContent = quantity.toString();
+
+        buttonHandler.updateProductInCart(1, dataToInsert.product_id, quantity) //TODO hardcode cartID
+    })
 
     quantityContainer.appendChild(minusButton);
     quantityContainer.appendChild(quantityValue);
     quantityContainer.appendChild(plusButton);
 
-    const totalCost = util.createElementWithClasses("p","total-cost");
+    const totalCost = util.createElementWithClasses("p", "total-cost");
     totalCost.innerText = " " + dataToInsert.subTotalPrice + " " + dataToInsert.currency + " ";
 
-    const removeButton = util.createElementWithClasses("a","remove-from-cart-button");
-    const removeIcon = util.createElementWithClasses("i","fas", "fa-trash-alt");
+    const removeButton = util.createElementWithClasses("a", "remove-from-cart-button");
+    const removeIcon = util.createElementWithClasses("i", "fas", "fa-trash-alt");
     removeButton.appendChild(removeIcon);
     removeButton.innerText = "Remove Item";
 
@@ -77,8 +97,8 @@ const addItemToDisplay = dataToInsert => {
 }
 
 const addTotalPriceContainer = totalPrice => {
-    const totalPriceBox = util.createElementWithClasses("div","card-details", "item", "flex-row");
-    const number = util.createElementWithClasses("p","number");
+    const totalPriceBox = util.createElementWithClasses("div", "card-details", "item", "flex-row");
+    const number = util.createElementWithClasses("p", "number");
     number.innerText = `Total price: ${totalPrice} `;
 
     totalPriceBox.appendChild(number);
