e7c55d2017c5c282593a0667eb9a0c4c16a76e67
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-29 13:27:28 +0100

    Add api_put method sending put request and Add some methods to get Address, update Address and postOrder

--- src/main/webapp/static/js/data_handler.js
+++ src/main/webapp/static/js/data_handler.js
@@ -24,6 +24,20 @@ export const dataHandler = {
             .then(json_response => callback(json_response))
     },
 
+    _api_put: function (url, data, callback) {
+        fetch(url, {
+            method: 'PUT',
+            credentials: 'same-origin',
+            body: JSON.stringify(data),
+            cache: "no-cache",
+            headers: new Headers({
+                "content-type": "application/json"
+            })
+        })
+            .then(response => response.json())
+            .then(json_response => callback(json_response))
+    },
+
     getAllProducts: function (callback) {
         this._api_get("api/products", response => {
             this._data['products'] = response;
@@ -55,6 +69,34 @@ export const dataHandler = {
     getCart: function (cartId, callback) {
         this._api_get("api/carts/" + cartId, response => {
             this._data['cart-details'] = response;
+            callback(response);
+        })
+    },
+
+    postCart: function (cartDetails, callback) {
+        this._api_post("api/carts", response => {
+            this._data['cart-details'] = response;
+            callback(response);
+        })
+    },
+
+    getAddress: function (id, callback) {
+        this._api_get("api/addresses/" + id, response => {
+            this._data['address-details'] = response;
+            callback(response);
+        })
+    },
+
+    updateAddress: function (newAddress, addressId, callback) {
+        this._api_put("api/addresses/" + addressId, response => {
+            this._data['address-details'] = response;
+            callback(response);
+        })
+    },
+
+    postOrder: function (orderDetails, callback) {
+        this._api_post("api/orders", response => {
+            this._data['order-details'] = response;
             callback(response);
         })
     }
