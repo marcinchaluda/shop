d84e2435e8b49f697b9f83f1bd6dd64e3ab09ea3
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-10-29 13:27:53 +0100

    Refactor cart_handler.js file to hold all necessary functionalities

--- src/main/webapp/static/js/cart_handler.js
+++ src/main/webapp/static/js/cart_handler.js
@@ -1,5 +1,4 @@
 import {cartButtonHandler} from "./cart_button_handler.js";
-import {cartFormHandler} from "./cart_form_handler.js";
 import {dataHandler} from "./data_handler.js";
 
 const itemsContainer = document.querySelector("#items");
@@ -8,34 +7,40 @@ const usernameTitleContainer = document.querySelector("#username-cart-title");
 const cartHandler = {
     activate: () => {
         generateItemsInBasket(1);
-        cartHandler.initAllButtonHandlers();
-    },
-    initAllButtonHandlers: () => {
-
-    },
-
+    }
 }
 
-const generateItemsInBasket = (cartId) => {
-    dataHandler.getCart(cartId, (response) => {
+const generateItemsInBasket = cartId => {
+    dataHandler.getCart(cartId, response => {
         createListOfItems(response);
+        cartButtonHandler.init(response);
     })
 }
 
-const createListOfItems = (data) => {
+const createListOfItems = data => {
+    let totalPrice = 0;
     for (let i = 0; i < data.products.length; i++) {
         const productDetails = data.products[i].product;
         const dataToInsert = {
             name: data.products[i].name,
             quantity: data.products[i].quantity,
-            totalPrice: data.products[i].quantity * productDetails.unitPrice
+            subTotalPrice: data.products[i].quantity * productDetails.unitPrice,
+            currency: productDetails.currency
         }
+        totalPrice += data.products[i].quantity * productDetails.unitPrice;
         addItemToDisplay(dataToInsert);
     }
     usernameTitleContainer.textContent = data.user.name + "'s cart";
+    addTotalPriceContainer(totalPrice + " " + data.products[0].product.currency);
 }
 
-const addItemToDisplay = (dataToInsert) => {
+const addTotalPriceContainer = totalPrice => {
+    itemsContainer.innerHTML += `<div class="card-details item flex-row">
+                        <p class="number">Total price: ` + totalPrice + `</p>
+                    </div>`
+}
+
+const addItemToDisplay = dataToInsert => {
     itemsContainer.innerHTML += `<div class="card-details item flex-row">
                         <p class="item-name">` + dataToInsert.name + `</p>
                         <div class="quantity flex-row">
@@ -43,7 +48,7 @@ const addItemToDisplay = (dataToInsert) => {
                             <p class="number">` + dataToInsert.quantity + `</p>
                             <a class="plus-minus" href="#" class="increase-quantity"><i class="fas fa-plus"></i></a>
                         </div>
-                        <p class="total-cost">` + dataToInsert.totalPrice + `</p>
+                        <p class="total-cost">` + dataToInsert.subTotalPrice + " " + dataToInsert.currency + `</p>
                         <a href="#" class="remove-from-cart-button"><i class="fas fa-trash-alt"></i>Remove Item</a>
                     </div>`
 }
