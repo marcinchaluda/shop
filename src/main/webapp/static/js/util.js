5006641f415e59503fa3e9acdcea54375b9dc37f
RobertMatlag <robert.matlag@gmail.com>
2020-10-29 19:42:35 +0100

    Refactor code: change inserting Html code to dynamic creating elements in JS

---
+++ src/main/webapp/static/js/util.js
@@ -0,0 +1,13 @@
+export const util = {
+    createElementWithClasses: function (element, ...args) {
+        const newElement = document.createElement(element);
+        args.forEach(arg => {
+            newElement.classList.add(arg);
+        });
+        return newElement;
+    },
+
+    removeContent: function (element) {
+        element.innerHTML = '';
+    }
+}
