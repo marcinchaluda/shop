97a5f451b4acdb3e9d2cfeaea29a2f08f603c9b4
marcinchaluda <chaluda@interia>
2020-11-10 18:15:32 +0100

    add login button and login servlet

---
+++ src/main/java/com/codecool/shop/api/LoginServlet.java
@@ -0,0 +1,21 @@
+package com.codecool.shop.api;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        engine.process("user/login.html", context, response.getWriter());
+    }
+}
