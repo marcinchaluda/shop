dc14a59f809518f3f0517b42bd3815feeec12015
marcinchaluda <chaluda@interia>
2020-11-11 13:36:26 +0100

    clear session after user logout

---
+++ src/main/java/com/codecool/shop/api/LogoutServlet.java
@@ -0,0 +1,30 @@
+package com.codecool.shop.api;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        clearSession(request);
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        engine.process("product/index.html", context, response.getWriter());
+    }
+
+    private void clearSession(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+    }
+}
