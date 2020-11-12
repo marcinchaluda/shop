d4025cdc8883f76f4d0c895ea0368406904dd2c7
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-11-12 11:35:04 +0100

    Add security to PaymentServlet

--- src/main/java/com/codecool/shop/sites/PaymentServlet.java
+++ src/main/java/com/codecool/shop/sites/PaymentServlet.java
@@ -1,6 +1,8 @@
 package com.codecool.shop.sites;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.logic.SessionLogic;
+import com.codecool.shop.model.User;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -8,19 +10,28 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/payment/*"})
 public class PaymentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
         String pathInfo = request.getPathInfo();
-        if (pathInfo == null || pathInfo.equals("/")) {
-            response.sendRedirect("/");
+        if (pathInfo == null || !pathInfo.contains("/") || session.getAttribute("user") == null) {
+            response.sendRedirect("/login");
         } else {
-            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
-            WebContext context = new WebContext(request, response, request.getServletContext());
-            engine.process("payment/payment.html", context, response.getWriter());
+            SessionLogic sessionLogic = SessionLogic.getInstance();
+            String[] splits = pathInfo.split("/");
+            User user = (User) session.getAttribute("user");
+            if (sessionLogic.orderBelongsToUser(user.getId(), Integer.parseInt(splits[1]))) {
+                TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+                WebContext context = new WebContext(request, response, request.getServletContext());
+                engine.process("payment/payment.html", context, response.getWriter());
+            } else {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            }
         }
     }
 }
