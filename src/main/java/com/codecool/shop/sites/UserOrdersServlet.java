95033e8d09627f367958fb88b29ab4c032c417ed
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-11-12 11:34:55 +0100

    Add security to UserOrdersServlet

--- src/main/java/com/codecool/shop/sites/UserOrdersServlet.java
+++ src/main/java/com/codecool/shop/sites/UserOrdersServlet.java
@@ -3,6 +3,7 @@ package com.codecool.shop.sites;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.logic.OrderLogic;
 import com.codecool.shop.model.OrderHistory;
+import com.codecool.shop.model.User;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -10,18 +11,29 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/orders"})
 public class UserOrdersServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        WebContext context = new WebContext(request, response, request.getServletContext());
-        int userId = Integer.parseInt(request.getParameter("user"));
-        OrderHistory orderHistory = OrderLogic.getInstance().getOrdersHistoryBy(userId);
-        context.setVariable("user", orderHistory.getUser());
-        context.setVariable("orders", orderHistory.getOrderList());
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
-        engine.process("order/order-list.html", context, response.getWriter());
+        HttpSession session = request.getSession();
+        if (session.getAttribute("user") == null) {
+            response.sendRedirect("/login");
+        } else {
+            User user = (User) session.getAttribute("user");
+            WebContext context = new WebContext(request, response, request.getServletContext());
+            int userId = Integer.parseInt(request.getParameter("user"));
+            if (userId == user.getId()) {
+                OrderHistory orderHistory = OrderLogic.getInstance().getOrdersHistoryBy(userId);
+                context.setVariable("user", orderHistory.getUser());
+                context.setVariable("orders", orderHistory.getOrderList());
+                TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+                engine.process("order/order-list.html", context, response.getWriter());
+            } else {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+        }
     }
 }
